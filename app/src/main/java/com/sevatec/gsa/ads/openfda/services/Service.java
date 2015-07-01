a40bbbdd47303c908a5f3472960c97646c23096a
Doug Crane <dcrane@sevatec.com>
2015-07-01 14:53:44 -0400

    working demo service

--- app/src/main/java/com/sevatec/gsa/ads/openfda/services/Service.java
+++ app/src/main/java/com/sevatec/gsa/ads/openfda/services/Service.java
@@ -3,6 +3,8 @@
  */
 package com.sevatec.gsa.ads.openfda.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -13,39 +15,45 @@ import javax.ws.rs.core.Response;
 @Path("/services")
 public class Service {
 
-    @GET
-    @Path("/getTags")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTags() {
-        return Response.ok().entity("here ya go: ").build();
-    }
+	@GET
+	@Path("/getTags")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTags() {
+		return Response.ok().entity("here ya go: ").build();
+	}
 
-    @GET
-    @Path("/getDrugNames/{drug}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getDrugNames(@PathParam("drug") String drug) {
-        return Response.ok().entity("here ya go: " + drug).build();
-    }
+	@GET
+	@Path("/getDrugNames/{drug}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getDrugNames(@PathParam("drug") String drug) {
 
-    @GET
-    @Path("/getLabel/{drug}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getLabel(@PathParam("drug") String drug) {
-	return Response.ok().entity("here is the label: " + drug).build();
-    }
+		Result result = new Result();
+		result.getNames().add(new Name("foo"));
+		result.getNames().add(new Name("bar"));
+		result.getNames().add(new Name("baz"));
+		result.getNames().add(new Name("doo"));
+		return Response.ok().entity(result).build();
+	}
 
-    @GET
-    @Path("/getAdverseEvents/{drug}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAdverseEvents(@PathParam("drug") String drug) {
-	return Response.ok().entity("here is the adverse events: " + drug).build();
-    }
+	@GET
+	@Path("/getLabel/{drug}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getLabel(@PathParam("drug") String drug) {
+		return Response.ok().entity("here is the label: " + drug).build();
+	}
 
-    @GET
-    @Path("/getEnforcementReports/{drug}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getEnforcementReports(@PathParam("drug") String drug) {
-	return Response.ok().entity("here is the enforcement reports: " + drug).build();
-    }
+	@GET
+	@Path("/getAdverseEvents/{drug}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAdverseEvents(@PathParam("drug") String drug) {
+		return Response.ok().entity("here is the adverse events: " + drug).build();
+	}
+
+	@GET
+	@Path("/getEnforcementReports/{drug}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getEnforcementReports(@PathParam("drug") String drug) {
+		return Response.ok().entity("here is the enforcement reports: " + drug).build();
+	}
 
 }
