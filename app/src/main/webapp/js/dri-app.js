13c4a3119b260cdd668462f30b9a4e4884158678
estenzelatsevatec <estenzel@sevatec.com>
2015-07-01 18:21:31 -0400

    added search again and new panel for results

--- app/src/main/webapp/js/dri-app.js
+++ app/src/main/webapp/js/dri-app.js
@@ -1,5 +1,21 @@
 var app = angular.module('dri-app', ["ngTouch", "angucomplete-alt", "ngRoute"]);
+var xmlHttp = null;
 
-app.controller("driAppCtrl", ['$scope', function($scope) {
-	$scope.greeting = "Hola!";
-}]);
+app.controller("dri-app-ctrl", ['$scope', function($scope) {
+	$scope.result = null;
+        $scope.debugtext = "";
+        
+        $scope.doGetFromServer = function( selected ) {
+            if (selected) {
+              $scope.result = selected.originalObject;
+            } else {
+              $scope.result = null;
+            }            
+        }
+
+        $scope.inputChanged = function(str) {
+            $scope.debugtext = "hope";
+        }
+    }
+]);
+
