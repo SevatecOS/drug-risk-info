2e638b160c06adffa47eeb3237d372956102bef9
Doug Crane <dcrane@sevatec.com>
2015-07-01 11:18:10 -0400

    fixed sample service, adding mongodb drivers in

--- app/src/main/java/com/sevatec/gsa/ads/openfda/services/Service.java
+++ app/src/main/java/com/sevatec/gsa/ads/openfda/services/Service.java
@@ -5,6 +5,7 @@ package com.sevatec.gsa.ads.openfda.services;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,10 +14,10 @@ import javax.ws.rs.core.Response;
 public class Service {
 
     @GET
-    @Path("/")
+    @Path("/getDrugNames/{drug}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getDrugNames(String drug) {
-        return Response.status(Response.Status.OK).entity("here ya go").build();
+    public Response getDrugNames(@PathParam("drug") String drug) {
+        return Response.status(Response.Status.OK).entity("here ya go: " + drug).build();
 
     }
 }
