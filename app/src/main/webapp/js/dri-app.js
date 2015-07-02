3a94bc74014c977c622667569a2a6bfecb183db9
Doug Crane <dcrane@sevatec.com>
2015-07-02 04:14:59 -0400

    actually calling the rest api for results

--- app/src/main/webapp/js/dri-app.js
+++ app/src/main/webapp/js/dri-app.js
@@ -1,23 +1,27 @@
 var app = angular.module('dri-app', ["ngTouch", "angucomplete-alt", "ngRoute"]);
 var xmlHttp = null;
 
-app.controller("dri-app-ctrl", ['$scope', function($scope) {
-	$scope.result = null;
-        $scope.debugtext = "";
-        
-        $scope.doGetFromServer = function( selected ) {
-            if (selected) {
-              var drugName = selected.originalObject.name;
-              // need to send request to rest/services/getDrugDetail/{drugName}
-              $scope.result = drugName;
-            } else {
-              $scope.result = null;
-            }            
-        }
+app.controller("dri-app-ctrl", function ($scope, $http) {
+    $scope.result = null;
+    $scope.debugtext = "";
 
-        $scope.inputChanged = function(str) {
-            $scope.debugtext = "hope";
+    $scope.doGetFromServer = function (selected) {
+        if (selected) {
+            var drugName = selected.originalObject.name;
+            $http.get('rest/services/getDrugDetail/' + drugName).
+                    success(function (data, status, headers, config) {
+                        $scope.result = data;
+                    }).
+                    error(function (data, status, headers, config) {
+                        $scope.result = data;
+                    });
+        } else {
+            $scope.result = null;
         }
-    }
-]);
+    };
 
+    $scope.inputChanged = function (str) {
+        $scope.debugtext = "hope";
+    };
+
+});
